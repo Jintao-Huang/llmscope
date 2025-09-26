414885172d0e9248074cc536946be369b1c08a7e
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-09-26 17:28:12 +0800

    update

--- swift/megatron/model/gpt/mcore2hf.py
+++ swift/megatron/model/gpt/mcore2hf.py
@@ -6,16 +6,16 @@ from torch import nn
 
 
 def set_mla_attn_state(args, mg_attn, hf_attn):
-    hf_attn.o_proj.weight.data.copy_(mg_attn.linear_proj.weight)
+    hf_attn.o_proj.weight = mg_attn.linear_proj.weight
     if args.q_lora_rank is None:
-        hf_attn.q_proj.weight.data.copy_(mg_attn.linear_q_proj.weight)
+        hf_attn.q_proj.weight = mg_attn.linear_q_proj.weight
     else:
-        hf_attn.q_a_proj.weight.data.copy_(mg_attn.linear_q_down_proj.weight)
-        hf_attn.q_b_proj.weight.data.copy_(mg_attn.linear_q_up_proj.weight)
-    hf_attn.kv_a_proj_with_mqa.weight.data.copy_(mg_attn.linear_kv_down_proj.weight)
-    hf_attn.kv_b_proj.weight.data.copy_(mg_attn.linear_kv_up_proj.weight)
+        hf_attn.q_a_proj.weight = mg_attn.linear_q_down_proj.weight
+        hf_attn.q_b_proj.weight = mg_attn.linear_q_up_proj.weight
+    hf_attn.kv_a_proj_with_mqa.weight = mg_attn.linear_kv_down_proj.weight
+    hf_attn.kv_b_proj.weight = mg_attn.linear_kv_up_proj.weight
     if args.qk_layernorm:
-        hf_attn.kv_a_layernorm.weight.data.copy_(mg_attn.linear_kv_up_proj.layer_norm_weight)
+        hf_attn.kv_a_layernorm.weight = mg_attn.linear_kv_up_proj.layer_norm_weight
 
 
 def set_attn_state(args, mg_attn, hf_attn):
@@ -24,32 +24,32 @@ def set_attn_state(args, mg_attn, hf_attn):
     mg_attn_weight = mg_attn.linear_qkv.weight.reshape((num_query_groups, -1, args.hidden_size))
     q_dim, kv_dim = hf_attn.q_proj.weight.shape[0] // num_query_groups, hf_attn.k_proj.weight.shape[
         0] // num_query_groups
-    hf_attn.q_proj.weight.data.copy_(mg_attn_weight[:, :q_dim, :].reshape(-1, args.hidden_size))
-    hf_attn.k_proj.weight.data.copy_(mg_attn_weight[:, q_dim:-kv_dim, :].reshape(-1, args.hidden_size))
-    hf_attn.v_proj.weight.data.copy_(mg_attn_weight[:, -kv_dim:, :].reshape(-1, args.hidden_size))
-    hf_attn.o_proj.weight.data.copy_(mg_attn.linear_proj.weight)
+    hf_attn.q_proj.weight = nn.Parameter(mg_attn_weight[:, :q_dim, :].reshape(-1, args.hidden_size))
+    hf_attn.k_proj.weight = nn.Parameter(mg_attn_weight[:, q_dim:-kv_dim, :].reshape(-1, args.hidden_size))
+    hf_attn.v_proj.weight = nn.Parameter(mg_attn_weight[:, -kv_dim:, :].reshape(-1, args.hidden_size))
+    hf_attn.o_proj.weight = mg_attn.linear_proj.weight
 
     # Copy bias
     if args.add_qkv_bias:
         mg_attn_bias = mg_attn.linear_qkv.bias.reshape((num_query_groups, -1))
-        hf_attn.q_proj.bias.data.copy_(mg_attn_bias[:, :q_dim].reshape(-1))
-        hf_attn.k_proj.bias.data.copy_(mg_attn_bias[:, q_dim:-kv_dim].reshape(-1))
-        hf_attn.v_proj.bias.data.copy_(mg_attn_bias[:, -kv_dim:].reshape(-1))
+        hf_attn.q_proj.bias = nn.Parameter(mg_attn_bias[:, :q_dim].reshape(-1))
+        hf_attn.k_proj.bias = nn.Parameter(mg_attn_bias[:, q_dim:-kv_dim].reshape(-1))
+        hf_attn.v_proj.bias = nn.Parameter(mg_attn_bias[:, -kv_dim:].reshape(-1))
 
     if args.qk_layernorm:
         q_norm = hf_attn.query_layernorm if hasattr(hf_attn, 'query_layernorm') else hf_attn.q_norm
         k_norm = hf_attn.key_layernorm if hasattr(hf_attn, 'key_layernorm') else hf_attn.k_norm
-        q_norm.weight.data.copy_(mg_attn.q_layernorm.weight)
-        k_norm.weight.data.copy_(mg_attn.k_layernorm.weight)
+        q_norm.weight = mg_attn.q_layernorm.weight
+        k_norm.weight = mg_attn.k_layernorm.weight
 
 
 def _set_moe_state(args, mg_mlp, hf_mlp):
     hf_gate = hf_mlp.gate
     if hasattr(hf_gate, 'wg'):
         hf_gate = hf_gate.wg
-    hf_gate.weight.data.copy_(mg_mlp.router.weight)
+    hf_gate.weight = mg_mlp.router.weight
     if args.moe_router_enable_expert_bias:
-        hf_gate.e_score_correction_bias.data.copy_(mg_mlp.router.expert_bias)
+        hf_gate.e_score_correction_bias = mg_mlp.router.expert_bias
     if mg_mlp.shared_experts is not None:
         if hasattr(hf_mlp, 'shared_experts'):
             hf_shared_expert = hf_mlp.shared_experts
@@ -59,7 +59,7 @@ def _set_moe_state(args, mg_mlp, hf_mlp):
             hf_shared_expert = hf_mlp.shared_expert
         _set_mlp_state(mg_mlp.shared_experts, hf_shared_expert)
         if mg_mlp.shared_experts.gate_weight is not None:
-            hf_mlp.shared_expert_gate.weight.data.copy_(mg_mlp.shared_experts.gate_weight)
+            hf_mlp.shared_expert_gate.weight = mg_mlp.shared_experts.gate_weight
     for expert_idx in range(args.num_experts):
         hf_expert = hf_mlp.experts
         if hasattr(hf_expert, '__len__'):
@@ -81,12 +81,12 @@ def _set_mlp_state(mg_mlp, hf_mlp, group_idx: Optional[int] = None):
         hf_mlp.down_proj.data[group_idx] = linear_fc2_weight.t()
     else:
         if hasattr(hf_mlp, 'gate_up_proj'):
-            hf_mlp.gate_up_proj.weight.data.copy_(linear_fc1_weight)
+            hf_mlp.gate_up_proj.weight = linear_fc1_weight
         else:
             ffn_hidden_size = hf_mlp.gate_proj.weight.shape[0]
-            hf_mlp.gate_proj.weight.data.copy_(linear_fc1_weight[:ffn_hidden_size])
-            hf_mlp.up_proj.weight.data.copy_(linear_fc1_weight[ffn_hidden_size:])
-        hf_mlp.down_proj.weight.data.copy_(linear_fc2_weight)
+            hf_mlp.gate_proj.weight = nn.Parameter(linear_fc1_weight[:ffn_hidden_size])
+            hf_mlp.up_proj.weight = nn.Parameter(linear_fc1_weight[ffn_hidden_size:])
+        hf_mlp.down_proj.weight = linear_fc2_weight
 
 
 def set_mlp_state(args, mg_mlp, hf_mlp):
@@ -102,26 +102,27 @@ def set_layer_state(args, mg_model, hf_model, layer_idx):
 
     if args.multi_latent_attention:
         set_mla_attn_state(args, mg_layer.self_attention, hf_layer.self_attn)
-        hf_layer.input_layernorm.weight.data.copy_(mg_layer.input_layernorm.weight)
+        hf_layer.input_layernorm.weight = mg_layer.input_layernorm.weight
     else:
         set_attn_state(args, mg_layer.self_attention, hf_layer.self_attn)
-        hf_layer.input_layernorm.weight.data.copy_(mg_layer.self_attention.linear_qkv.layer_norm_weight)
+        hf_layer.input_layernorm.weight = mg_layer.self_attention.linear_qkv.layer_norm_weight
 
     set_mlp_state(args, mg_layer.mlp, hf_layer.mlp)
 
-    post_attention_layernorm_weight = hf_layer.post_attention_layernorm.weight
     if 'moe' in mg_layer.mlp.__class__.__name__.lower():
-        post_attention_layernorm_weight.data.copy_(mg_layer.pre_mlp_layernorm.weight)
+        hf_layer.post_attention_layernorm.weight = mg_layer.pre_mlp_layernorm.weight
     else:
-        post_attention_layernorm_weight.data.copy_(mg_layer.mlp.linear_fc1.layer_norm_weight)
+        hf_layer.post_attention_layernorm.weight = mg_layer.mlp.linear_fc1.layer_norm_weight
 
 
 def convert_mcore2hf(hf_model, mg_model):
     args = get_args()
-    hf_model.model.embed_tokens.weight.data.copy_(mg_model.embedding.word_embeddings.weight)
+    hf_model.model.embed_tokens.weight = mg_model.embedding.word_embeddings.weight
+    lm_head = hf_model.score if args.task_type == 'seq_cls' else hf_model.lm_head
     if args.untie_embeddings_and_output_weights:
-        lm_head_weight = hf_model.score.weight if args.task_type == 'seq_cls' else hf_model.lm_head.weight
-        lm_head_weight.data.copy_(mg_model.output_layer.weight)
-    hf_model.model.norm.weight.data.copy_(mg_model.decoder.final_layernorm.weight)
+        lm_head.weight = mg_model.output_layer.weight
+    else:
+        lm_head.weight = hf_model.model.embed_tokens.weight
+    hf_model.model.norm.weight = mg_model.decoder.final_layernorm.weight
     for layer_idx in range(args.num_layers):
         set_layer_state(args, mg_model, hf_model.model, layer_idx)
