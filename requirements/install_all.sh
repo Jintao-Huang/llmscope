a31a97eeffb68330731524f22ee6cf81a5e83cef
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-02-22 18:14:48 +0800

    update

--- requirements/install_all.sh
+++ requirements/install_all.sh
@@ -4,7 +4,7 @@ pip install sglang -U
 pip install "vllm>=0.5.1" -U
 pip install "lmdeploy>=0.5,<0.10.2" -U --no-deps
 pip install transformers trl peft -U
-pip install auto_gptq optimum bitsandbytes gradio -U
+pip install auto_gptq optimum bitsandbytes "gradio<5.33" -U
 pip install git+https://github.com/modelscope/ms-swift.git#egg=ms-swift[all]
 pip install timm "deepspeed<0.19" -U
 pip install qwen_vl_utils qwen_omni_utils keye_vl_utils -U
